9af4420ece5f6dde1e442c76fa2660af90598158
beatrizbozzo <beatrizbblc@al.insper.edu.br>
2020-05-12 14:28:07 -0300

    Plataforma

--- Jogo.py
+++ Jogo.py
@@ -14,10 +14,14 @@ pygame.display.set_caption('Super Maria Sis')
 # ----- Inicia assets
 ESPINHO_LARGURA = 50
 ESPINHO_ALTURA = 38
+PLATAFORMA_LARGURA = 480
+PLATAFORMA_ALTURA = 50
 font = pygame.font.SysFont(None, 48)
-fundo = pygame.image.load('assets/fundo.jpg').convert()
+fundo = pygame.image.load('assets/fundo2.png').convert()
 espinho_img = pygame.image.load('assets/espinho.png').convert_alpha()
 espinho_img = pygame.transform.scale(espinho_img, (ESPINHO_LARGURA, ESPINHO_ALTURA))
+plataforma_img = pygame.image.load('assets/plataforma.png').convert_alpha()
+plataforma_img = pygame.transform.scale(plataforma_img, (PLATAFORMA_LARGURA, PLATAFORMA_ALTURA))
  
 # ----- Inicia estruturas de dados
 # Definindo os novos tipos
@@ -25,19 +29,18 @@ class Espinho(pygame.sprite.Sprite):
     def __init__(self, img):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
- 
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, LARGURA-ESPINHO_LARGURA)
         self.rect.y = random.randint(-100, -ESPINHO_ALTURA)
         self.velocidadex = random.randint(-3, 3)
         self.velocidadey = random.randint(2, 9)
- 
+
     def update(self):
         # Atualizando a posição do espinho
         self.rect.x += self.velocidadex
         self.rect.y += self.velocidadey
-        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # Se o espinho passar do final da tela, volta para cima e sorteia
         # novas posições e velocidades
         if self.rect.top > ALTURA or self.rect.right < 0 or self.rect.left > LARGURA:
             self.rect.x = random.randint(0, LARGURA-ESPINHO_LARGURA)
@@ -45,29 +48,35 @@ class Espinho(pygame.sprite.Sprite):
             self.velocidadex = random.randint(-3, 3)
             self.velocidadey = random.randint(2, 9)
 
+class Plataforma(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.bottom = ALTURA - 10
 
 game = True
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 FPS = 30
 
-
 # Criando os espinhos
 todos_espinhos = pygame.sprite.Group()
 for i in range(6):
     espinho = Espinho(espinho_img)
     todos_espinhos.add(espinho)
+
+plataforma = Plataforma(plataforma_img)
  
 # ===== Loop principal =====
 while game:
     clock.tick(FPS)
- 
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
- 
     # ----- Atualiza estado do jogo
     # Atualizando a posição dos espinhos
     todos_espinhos.update()
@@ -78,6 +87,7 @@ while game:
 
     # Desenhando espinhos
     todos_espinhos.draw(window)
+    window.blit(plataforma.image, plataforma.rect)
     pygame.display.update()
 
  
